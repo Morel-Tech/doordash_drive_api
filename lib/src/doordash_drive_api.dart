1d733a954c7020f4f1499604b51a8f53cc671c37
Bettina Carrizo <betticarrizo@gmail.com>
2024-02-09 16:14:12 -0300

    feat: Add Update & Cancel Delivery endpoints w/Tests

--- lib/src/doordash_drive_api.dart
+++ lib/src/doordash_drive_api.dart
@@ -1,5 +1,6 @@
 import 'package:doordash_drive_api/doordash_drive_api.dart';
 import 'package:doordash_drive_api/src/doordash_drive_client.dart';
+import 'package:doordash_drive_api/src/functions_model/update_delivery.dart';
 
 /// {@template doordash_drive_api}
 /// A client for the Doordash Drive REST API
@@ -15,58 +16,112 @@ class DoordashDriveApi {
   /// Confirm that a delivery is serviceable by DoorDash and
   /// what it would cost by creating a quote.
   /// Get a quote on delivery fee and validate coverage.
-  /// [DeliveryRequest] : Request body details of the endpoint call
+  ///
+  /// @see: https://developer.doordash.com/en-US/api/drive/#tag/Delivery/operation/DeliveryQuote
+  ///
+  /// [request] : Request body details of the endpoint call
   Future<Delivery> createQuote({
     required DeliveryRequest request,
-  }) =>
-      _client.send(
-        path: DoordashDriveEndpointPaths.createQuote,
-        method: HttpMethod.post,
-        body: request.toJson(),
-        responseFromJson: Delivery.fromJson,
-      );
+  }) async {
+    final response = await _client.send(
+      path: DoordashDriveEndpointPaths.createQuote,
+      method: HttpMethod.post,
+      body: request.toJson(),
+    );
+
+    return Delivery.fromJson(response);
+  }
 
   /// When you’re happy with the quote you created, start the delivery process
   /// by accepting the quote.
   ///
+  /// @see: https://developer.doordash.com/en-US/api/drive/#tag/Delivery/operation/DeliveryQuoteAccept
+  ///
   /// [externalDeliveryId] : Unique (per developer) ID of the delivery.
-  /// [AcceptQuoteRequest] : Request body details of the endpoint call
+  /// [request] : Request body details of the endpoint call
   Future<Delivery> acceptQuote({
     required String externalDeliveryId,
     required AcceptQuoteRequest request,
-  }) =>
-      _client.send(
-        path: DoordashDriveEndpointPaths.acceptQuote(externalDeliveryId),
-        method: HttpMethod.post,
-        body: request.toJson(),
-        responseFromJson: Delivery.fromJson,
-      );
+  }) async {
+    final response = await _client.send(
+      path: DoordashDriveEndpointPaths.acceptQuote(externalDeliveryId),
+      method: HttpMethod.post,
+      body: request.toJson(),
+    );
+
+    return Delivery.fromJson(response);
+  }
 
   /// Create a [Delivery]
-  /// [DeliveryRequest] : Request body details of the endpoint call
+  ///
+  /// @see: https://developer.doordash.com/en-US/api/drive/#tag/Delivery/operation/CreateDelivery
+  ///
+  /// [request] : Request body details of the endpoint call
   Future<Delivery> createDelivery({
     required DeliveryRequest request,
-  }) =>
-      _client.send(
-        path: DoordashDriveEndpointPaths.createDelivery,
-        method: HttpMethod.post,
-        body: request.toJson(),
-        responseFromJson: Delivery.fromJson,
-      );
+  }) async {
+    final response = await _client.send(
+      path: DoordashDriveEndpointPaths.createDelivery,
+      method: HttpMethod.post,
+      body: request.toJson(),
+    );
+
+    return Delivery.fromJson(response);
+  }
 
   /// Get the status and details of a delivery that you created.
   /// If you want to proactively receive updates about your delivery,
   /// set up webhooks using the webhooks how-to guide.
+  ///
   /// @see: https://developer.doordash.com/en-US/docs/drive/how_to/webhooks
+  /// @see: https://developer.doordash.com/en-US/api/drive/#tag/Delivery/operation/GetDelivery
+  ///
   /// [externalDeliveryId] : Unique (per developer) ID of the delivery.
   Future<Delivery> getDelivery({
     required String externalDeliveryId,
-  }) =>
-      _client.send(
-        path: DoordashDriveEndpointPaths.getDelivery(externalDeliveryId),
-        method: HttpMethod.get,
-        responseFromJson: Delivery.fromJson,
-      );
+  }) async {
+    final response = await _client.send(
+      path: DoordashDriveEndpointPaths.getUpdateDelivery(externalDeliveryId),
+      method: HttpMethod.get,
+    );
+
+    return Delivery.fromJson(response);
+  }
+
+  /// Update the details of a delivery. Only the fields listed below
+  /// can be updated; to update other fields, you need to cancel the
+  /// delivery and then create a new one.
+  ///
+  /// @see: https://developer.doordash.com/en-US/api/drive/#tag/Delivery/operation/UpdateDelivery
+  ///
+  /// [externalDeliveryId] : Unique (per developer) ID of the delivery.
+  Future<Delivery> updateDelivery({
+    required String externalDeliveryId,
+    required UpdateDeliveryRequest request,
+  }) async {
+    final response = await _client.send(
+      path: DoordashDriveEndpointPaths.getUpdateDelivery(externalDeliveryId),
+      method: HttpMethod.patch,
+      body: request.toJson(),
+    );
+    return Delivery.fromJson(response);
+  }
+
+  /// Cancel a delivery.
+  /// Deliveries can't be cancelled after a Dasher is assigned.
+  ///
+  /// @see: https://developer.doordash.com/en-US/api/drive/#tag/Delivery/operation/UpdateDelivery
+  ///
+  /// [externalDeliveryId] : Unique (per developer) ID of the delivery.
+  Future<Delivery> cancelDelivery({
+    required String externalDeliveryId,
+  }) async {
+    final response = await _client.send(
+      path: DoordashDriveEndpointPaths.cancelDelivery(externalDeliveryId),
+      method: HttpMethod.put,
+    );
+    return Delivery.fromJson(response);
+  }
 }
 
 abstract class DoordashDriveEndpointPaths {
@@ -83,6 +138,10 @@ abstract class DoordashDriveEndpointPaths {
   static const String createDelivery = '$_basePath/deliveries';
 
   /// /drive/v2/deliveries/[externalDeliveryId]
-  static String getDelivery(String externalDeliveryId) =>
+  static String getUpdateDelivery(String externalDeliveryId) =>
       '$_basePath/deliveries/$externalDeliveryId';
+
+  /// /drive/v2/deliveries/[externalDeliveryId]/cancel
+  static String cancelDelivery(String externalDeliveryId) =>
+      '$_basePath/deliveries/$externalDeliveryId/cancel';
 }
---
+++ lib/src/functions_model/update_delivery.dart
@@ -0,0 +1,209 @@
+import 'package:doordash_drive_api/doordash_drive_api.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'update_delivery.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class UpdateDeliveryRequest extends Equatable {
+  const UpdateDeliveryRequest({
+    this.pickupAddress,
+    this.pickupBusinessName,
+    this.pickupPhoneNumber,
+    this.pickupInstructions,
+    this.pickupExternalBusinessId,
+    this.pickupExternalStoreId,
+    this.dropoffAddress,
+    this.dropoffBusinessName,
+    this.dropoffLocation,
+    this.dropoffPhoneNumber,
+    this.dropoffInstructions,
+    this.dropoffContactGivenName,
+    this.dropoffContactFamilyName,
+    this.dropoffContactSendNotifications,
+    this.dropoffOptions,
+    this.contactlessDropoff,
+    this.actionIfUndeliverable,
+    this.tip,
+    this.orderContains,
+    this.dasherAllowedVehicles,
+    this.dropoffRequiresSignature,
+    this.promotionId,
+    this.dropoffCashOnDelivery,
+    this.orderValue,
+    this.items,
+    this.pickupTime,
+    this.dropoffTime,
+    this.pickupWindow,
+    this.dropoffWindow,
+  });
+
+  factory UpdateDeliveryRequest.fromJson(Map<String, dynamic> json) =>
+      _$UpdateDeliveryRequestFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UpdateDeliveryRequestToJson(this);
+
+  /// Comma-separated full address, in the order appropriate for your locale.
+  /// Overridden by store properties if
+  /// pickup_external_store_id and pickup_external_business_id are used.
+  final String? pickupAddress;
+
+  /// Optional name of the place, to help Dashers find the location.
+  /// Overridden by store properties if
+  /// pickup_external_store_id and pickup_external_business_id are used.
+  final String? pickupBusinessName;
+
+  /// The phone number for the Dasher to call if there are any issues with
+  /// the pick up. Should include the country code and must match the country
+  /// of the store for which the delivery is created. Overridden by store
+  /// properties if pickup_external_store_id and pickup_external_business_id
+  /// are used. Must adhere to E.164 international phone number standard.
+  final String? pickupPhoneNumber;
+
+  /// Instructions for the Dasher to follow when picking up the order.
+  final String? pickupInstructions;
+
+  /// The string identifier used to create your business. Used in combination with
+  /// pickup_external_store_id to select the pick up location.
+  /// If you provide an pickup_external_business_id,
+  /// you must also provide an pickup_external_store_id.
+  final String? pickupExternalBusinessId;
+
+  /// The string identifier used to create your store.
+  /// If you provide an pickup_external_store_id,
+  /// you must also provide an pickup_external_business_id.
+  /// If you provide an pickup_external_store_id, the address of the store
+  /// will override the pickup_address in the request.
+  final String? pickupExternalStoreId;
+
+  /// Comma-separated full address, in the order appropriate for your locale.
+  final String? dropoffAddress;
+
+  /// Optional name of the place, to help Dashers find the location.
+  final String? dropoffBusinessName;
+
+  /// (Pre-release field: submit a ticket in the Developer Portal or reach
+  /// out to your DoorDash contact if you would like to provide precise
+  /// location information.) The precise location, as latitude and longitude,
+  /// of the drop-off. If your request includes both location and address,
+  /// location will be used for Dasher navigation only; address will be used
+  /// for fee and serviceability checks.
+  final Location? dropoffLocation;
+
+  /// The phone number for the Dasher to call if there are any issues with the
+  /// delivery. Should include the country code.
+  /// Must adhere to E.164 international phone number standard
+  final String? dropoffPhoneNumber;
+
+  /// Instructions for the Dasher to follow when picking up the order.
+  final String? dropoffInstructions;
+
+  /// Given/first name of the contact.
+  final String? dropoffContactGivenName;
+
+  /// Family/last name of the contact.
+  final String? dropoffContactFamilyName;
+
+  /// Whether the contact will receive notifications from DoorDash for this
+  /// delivery. The default is true.
+  final bool? dropoffContactSendNotifications;
+
+  /// Additional options for drop off.
+  final DropoffOptions? dropoffOptions;
+
+  /// Whether the delivery should be contactless, which prompts a Dasher to take
+  /// a picture of the delivery at drop-off.
+  final bool? contactlessDropoff;
+
+  /// What the Dasher should do if the delivery is undeliverable.
+  /// The default is "dispose".
+  /// Other: "return_to_pickup"
+  /// See Create return to pickup deliveries for more information.
+  /// https://developer.doordash.com/en-US/docs/drive/how_to/return_to_pickup/
+  final ActionIfUndeliverable? actionIfUndeliverable;
+
+  /// The tip amount. Use cents or the equivalent lowest currency denomination
+  /// (e.g. $5.99 = 599).
+  final int? tip;
+
+  /// An object that specifies the restricted item(s) contained in this order.
+  final OrderContains? orderContains;
+
+  /// The vehicle type(s) that a Dasher can use to complete this delivery.
+  /// The default is "car".
+  /// Other: "bicycle" "walking"
+  final List<DasherAllowedVehicle>? dasherAllowedVehicles;
+
+  /// Whether the delivery requires signature verification during drop-off.
+  final bool? dropoffRequiresSignature;
+
+  /// The ID of the promotion that you want to apply to the delivery. If the
+  /// promotion ID is valid and the promotion is active, the response fee will
+  /// be adjusted according to the details of the promotion. Create a support
+  /// ticket to setup a promotion or if your promotion ID isn't working.
+  final String? promotionId;
+
+  /// The cash to collect when this order is dropped off, value in the lowest
+  /// currency denomination (e.g. cents). i.e. $19.99 = 1999.
+  /// (Pre-release field: Submit a ticket or reach out to your DoorDash contact
+  /// if you would like to accept cash on delivery.)
+  final int? dropoffCashOnDelivery;
+
+  /// The subtotal for all items in the order, excluding tax/tip,
+  /// in the lowest currency denomination (e.g. cents). i.e. $19.99 = 1999.
+  final int? orderValue;
+
+  /// The items included in the order.
+  final List<DeliveryItem>? items;
+
+  /// Time details in ISO-8601 format.
+  final String? pickupTime;
+
+  /// Time details in ISO-8601 format.
+  final String? dropoffTime;
+
+  /// The UTC date-time (in ISO-8601 format) overrides dropoff_time and
+  /// pickup_time when all four parameters of the time window
+  /// (pickup_window_start_time , pickup_window_end_time ,
+  /// dropoff_window_start_time , dropoff_window_end_time) are provided.
+  final TimeWindowDetails? pickupWindow;
+
+  /// The UTC date-time (in ISO-8601 format) overrides dropoff_time and
+  /// pickup_time when all four parameters of the time window
+  /// (pickup_window_start_time , pickup_window_end_time ,
+  /// dropoff_window_start_time , dropoff_window_end_time) are provided.
+  final TimeWindowDetails? dropoffWindow;
+
+  @override
+  List<Object?> get props => [
+        pickupAddress,
+        pickupBusinessName,
+        pickupPhoneNumber,
+        pickupInstructions,
+        pickupExternalBusinessId,
+        pickupExternalStoreId,
+        dropoffAddress,
+        dropoffBusinessName,
+        dropoffLocation,
+        dropoffPhoneNumber,
+        dropoffInstructions,
+        dropoffContactGivenName,
+        dropoffContactFamilyName,
+        dropoffContactSendNotifications,
+        dropoffOptions,
+        contactlessDropoff,
+        actionIfUndeliverable,
+        tip,
+        orderContains,
+        dasherAllowedVehicles,
+        dropoffRequiresSignature,
+        promotionId,
+        dropoffCashOnDelivery,
+        orderValue,
+        items,
+        pickupTime,
+        dropoffTime,
+        pickupWindow,
+        dropoffWindow,
+      ];
+}
