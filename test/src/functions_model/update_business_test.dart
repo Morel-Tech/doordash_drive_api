f78c333b1b85b02ce60db29eb824223280d3585a
Bettina Carrizo <betticarrizo@gmail.com>
2024-02-19 09:36:24 -0300

    feat: Business - Get & Update endpoints - Tests

--- test/src/functions_model/update_business_test.dart
+++ test/src/functions_model/update_business_test.dart
@@ -1,1 +1,29 @@
-void main() {}
+import 'package:doordash_drive_api/doordash_drive_api.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  group('UpdateBusinessRequest', () {
+    test('maps to object successfully', () {
+      const expected = UpdateBusinessRequest(
+        name: 'Neighborhood Deli',
+        description:
+            'A neighborhood deli serving many tasty sandwiches and soups.',
+        activationStatus: 'active',
+      );
+      final result = UpdateBusinessRequest.fromJson(
+        UpdateBusinessTestData.requestJson,
+      );
+      expect(result, expected);
+    });
+  });
+}
+
+abstract class UpdateBusinessTestData {
+  static const requestJson = {
+    'name': 'Neighborhood Deli',
+    'description':
+        'A neighborhood deli serving many tasty sandwiches and soups.',
+    'activation_status': 'active',
+  };
+}
